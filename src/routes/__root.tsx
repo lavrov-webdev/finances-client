5f54c1605ce2365e820a251d8f544d6c60ae5d56
Dmitry Lavrov <lavrov.webdev@gmail.com>
2024-08-30 15:01:24 +0300

    alias

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,12 +1,14 @@
 import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 import { TanStackRouterDevtools } from '@tanstack/router-devtools'
 import App from '../App'
+import { Component } from '@modules/mod'
 
 export const Route = createRootRoute({
     component: () => (
         <>
             <div className="p-2 flex gap-2">
                 <App />
+                <Component />
             </div>
             <hr />
             <Outlet />
