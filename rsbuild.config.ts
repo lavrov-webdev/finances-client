4257a0ee1e1538ebcc27ae857fb1972b11bda31c
Dmitry Lavrov <lavrov.webdev@gmail.com>
2024-08-30 15:19:26 +0300

    rsbuild_config

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,5 +3,24 @@ import { pluginReact } from '@rsbuild/plugin-react';
 import { pluginSass } from '@rsbuild/plugin-sass'
 
 export default defineConfig({
-  plugins: [pluginReact(), pluginSass()],
+plugins: [pluginReact(), pluginSass()],
+  output: {
+    polyfill: "usage"
+  },
+  html: {
+    title: "Finowise",
+    favicon: "./src/assets/favicon.ico"
+  },
+  environments: {
+    web: {
+      source: {
+        alias: {
+          "@components": "./src/components",
+          "@system": "./src/system",
+          "@layouts": "./src/layouts",
+          "@modules": "./src/modules",
+        }
+      }
+    }
+  }
 });
