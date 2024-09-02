5f270d8c5186dfb9a9e0e4049f0f8c406873f8ea
Dmitry Lavrov <lavrov.webdev@gmail.com>
2024-09-02 15:05:27 +0300

    new api path

--- src/system/axios.ts
+++ src/system/axios.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export const appAxios = axios.create({
   baseURL: process.env.NODE_ENV === 'development'
     ? "http://localhost:3001"
-    : "https://bbab1ktgch5jg0dbuk5v.containers.yandexcloud.net",
+    : "/api",
   withCredentials: true,
 });
